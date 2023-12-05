9e9eab4659c72421c310ae2567c305530b6b4720
iudchenko <andrii.iudchenko@gmail.com>
2023-12-05 19:00:55 +0100

    feat: add basic validation on signup

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -11,10 +11,33 @@ const SignUpPage = (): JSX.Element => {
 
   const navigate = useNavigate();
 
+  const isStrongPassword = (password: string): boolean => {
+    // Add your password strength validation logic here
+    // Minimum 8 characters, at least one letter, one digit, one special character
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    // Simple email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   const register = () => {
     if (!name) alert('Please enter name');
     if (!email) alert('Please enter email');
     if (!password) alert('Please enter password');
+    if (!isValidEmail(email)) {
+      alert('Please enter a valid email address');
+      return;
+    }
+    if (!isStrongPassword(password)) {
+      alert(
+        'Password must be at least 8 characters long and contain at least one letter, one digit, and one special character.',
+      );
+    }
     registerWithEmailAndPassword(name, email, password);
   };
 
