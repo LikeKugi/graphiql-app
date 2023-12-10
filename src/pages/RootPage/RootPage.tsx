271f53a09b106ddf1ed4cb2b697c59b29e31a19b
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-12-10 08:26:54 +0300

    refactor: change root layout

--- src/pages/RootPage/RootPage.tsx
+++ src/pages/RootPage/RootPage.tsx
@@ -2,14 +2,17 @@ import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import { JSX } from 'react';
 import { Outlet } from 'react-router-dom';
+import styles from './RootPage.module.scss';
 
 const RootPage = (): JSX.Element => {
   return (
-    <>
+    <div className={styles.root}>
       <Header />
-      <Outlet />
+      <div className={styles.root__content}>
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 export default RootPage;
