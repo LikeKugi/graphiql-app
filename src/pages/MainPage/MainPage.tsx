89856ccd66fa3b5790c4c88ce278a4fd0e25d782
iudchenko <andrii.iudchenko@gmail.com>
2023-12-14 14:13:35 +0100

    feat: localization improved

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,6 @@
-// import { useLanguage } from '@/contexts/LanguageContext';
 import { JSX } from 'react';
 
 const MainPage = (): JSX.Element => {
-  // const { t } = useLanguage();
   return <div>Main Page</div>;
 };
 export default MainPage;
