418a9ee9d19c661328eaef848dc0ecac6097bdf8
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-12-15 20:24:22 +0300

    feat: add main page

--- src/components/PlayGround/PlayGround.tsx
+++ src/components/PlayGround/PlayGround.tsx
@@ -1,50 +1,43 @@
-import { JSX, useState } from 'react';
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import { Dispatch, FC, JSX, SetStateAction, useState } from 'react';
+import { Panel, PanelGroup } from 'react-resizable-panels';
 import styles from './PlayGround.module.scss';
-import { Box, Chip, Divider, Tab, Tabs } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import SwapVertIcon from '@mui/icons-material/SwapVert';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import { graphql } from 'cm6-graphql';
 import { EditorView } from '@codemirror/view';
 import { langs } from '@uiw/codemirror-extensions-langs';
 import CodeMirror from '@uiw/react-codemirror';
+import PlayGroundResizer from '@/components/PlayGroundResizer/PlayGroundResizer';
 
-const PlayGround = (): JSX.Element => {
+interface IPlayGroundProps {
+  graphRequest: string;
+  headersRequest: string;
+  variablesRequest: string;
+  jsonResponse: string;
+  setGraphRequest: Dispatch<SetStateAction<string>>;
+  setHeadersRequest: Dispatch<SetStateAction<string>>;
+  setVariablesRequest: Dispatch<SetStateAction<string>>;
+}
+
+const PlayGround: FC<IPlayGroundProps> = ({
+  headersRequest,
+  setHeadersRequest,
+  variablesRequest,
+  setVariablesRequest,
+  graphRequest,
+  setGraphRequest,
+  jsonResponse,
+}): JSX.Element => {
   const [currentTab, setCurrentTab] = useState(0);
-  const [graphRequest, setGraphRequest] = useState('');
-  const [variablesRequest, setVariablesRequest] = useState('');
-  const [headersRequest, setHeadersRequest] = useState('');
-  const [jsonResponse, setJsonResponse] = useState('');
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     event.preventDefault();
     setCurrentTab(newValue);
   };
 
-  const handleSubmit = async () => {
-    setJsonResponse('');
-    const address = 'https://rickandmortyapi.com/graphql';
-    const body = JSON.stringify({
-      query: graphRequest,
-      variables: JSON.parse(variablesRequest),
-    })
-      .replace(/\\n/g, '')
-      .replace(/\s+/g, ' ');
-    const headers = headersRequest ? JSON.parse(headersRequest) : '';
-
-    const response = await fetch(address, {
-      method: 'POST',
-      headers: {
-        ...headers,
-        'Content-Type': 'application/json',
-      },
-      body,
-    });
-    const data = await response.json();
-    setJsonResponse(JSON.stringify(data, null, 2));
-  };
   return (
     <>
-      <button onClick={handleSubmit}>Go</button>
       <PanelGroup direction={'horizontal'} className={styles.PlayGround}>
         <Panel className={styles.PlayGround__panel} minSize={5} collapsible>
           <PanelGroup direction={'vertical'}>
@@ -59,35 +52,17 @@ const PlayGround = (): JSX.Element => {
                 extensions={[graphql(), EditorView.lineWrapping]}
               />
             </Panel>
-            <PanelResizeHandle>
-              <Divider>
-                <Chip
-                  label={<SwapVertIcon fontSize="small" />}
-                  variant="outlined"
-                />
-              </Divider>
-            </PanelResizeHandle>
+            <PlayGroundResizer label={<SwapVertIcon fontSize="small" />} />
             <Tabs value={currentTab} onChange={handleChange}>
-              <Tab label={'Headers'} />
               <Tab label={'Variables'} />
+              <Tab label={'Headers'} />
             </Tabs>
             <Panel
               className={styles.PlayGround__panel}
               minSize={10}
               collapsible
             >
-              <Box hidden={currentTab !== 0} p={0.5} flexGrow={1}>
-                <CodeMirror
-                  className={styles.PlayGround__CodeMirror}
-                  value={headersRequest}
-                  height="100%"
-                  onChange={(value) => {
-                    setHeadersRequest(value);
-                  }}
-                  extensions={[langs.json(), EditorView.lineWrapping]}
-                />
-              </Box>
-              <Box hidden={currentTab !== 1} p={1} flexGrow={1}>
+              <Box hidden={currentTab !== 0} p={1} flexGrow={1}>
                 <CodeMirror
                   className={styles.PlayGround__CodeMirror}
                   value={variablesRequest}
@@ -98,17 +73,24 @@ const PlayGround = (): JSX.Element => {
                   extensions={[langs.json(), EditorView.lineWrapping]}
                 />
               </Box>
+              <Box hidden={currentTab !== 1} p={0.5} flexGrow={1}>
+                <CodeMirror
+                  className={styles.PlayGround__CodeMirror}
+                  value={headersRequest}
+                  height="100%"
+                  onChange={(value) => {
+                    setHeadersRequest(value);
+                  }}
+                  extensions={[langs.json(), EditorView.lineWrapping]}
+                />
+              </Box>
             </Panel>
           </PanelGroup>
         </Panel>
-        <PanelResizeHandle>
-          <Divider orientation={'vertical'}>
-            <Chip
-              label={<SwapHorizIcon fontSize="small" />}
-              variant="outlined"
-            />
-          </Divider>
-        </PanelResizeHandle>
+        <PlayGroundResizer
+          label={<SwapHorizIcon fontSize="small" />}
+          orientation="vertical"
+        />
         <Panel minSize={5} collapsible className={styles.PlayGround__panel}>
           <CodeMirror
             className={styles.PlayGround__CodeMirror}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,79 @@
-import { JSX } from 'react';
+import { JSX, useState } from 'react';
 import PlayGround from '@/components/PlayGround/PlayGround';
+import PlayGroundActions from '@/components/PlayGroundActions/PlayGroundActions';
+import { prettifyJSON } from '@/utils/prettifyJSON';
 
 const MainPage = (): JSX.Element => {
+  const [graphRequest, setGraphRequest] = useState('');
+  const [variablesRequest, setVariablesRequest] = useState('');
+  const [headersRequest, setHeadersRequest] = useState('');
+  const [jsonResponse, setJsonResponse] = useState('');
+  const [urlAddress, setUrlAddress] = useState(
+    'https://rickandmortyapi.com/graphql',
+  );
+
+  const handleSubmit = async () => {
+    console.log('handle submit ---> start');
+    if (!graphRequest) {
+      return;
+    }
+    setJsonResponse('');
+    const address = urlAddress || 'https://rickandmortyapi.com/graphql';
+    let variables: object;
+    try {
+      variables = JSON.parse(variablesRequest);
+    } catch (e) {
+      variables = {};
+    }
+    const body = JSON.stringify({
+      query: graphRequest,
+      variables,
+    })
+      .replace(/\\n/g, '')
+      .replace(/\s+/g, ' ');
+    const headers = headersRequest ? JSON.parse(headersRequest) : '';
+
+    const response = await fetch(address, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+      body,
+    });
+    const data = await response.json();
+    setJsonResponse(prettifyJSON(data));
+  };
+
+  const handlePrettify = () => {
+    if (variablesRequest) {
+      setVariablesRequest(prettifyJSON(variablesRequest));
+    }
+    if (headersRequest) {
+      setHeadersRequest(prettifyJSON(headersRequest));
+    }
+    if (jsonResponse) {
+      setJsonResponse(prettifyJSON(jsonResponse));
+    }
+  };
+
   return (
     <div>
-      <h1>Main Page</h1>
-      <div>
-        <div>
-          <button>go</button>
-          <button>pretty</button>
-          <label htmlFor="uri">URI:</label>
-          <input id={'uri'} type="text" />
-        </div>
-      </div>
-      <PlayGround />
+      <PlayGroundActions
+        handleSubmit={handleSubmit}
+        handlePrettify={handlePrettify}
+        urlAddress={urlAddress}
+        setUrlAddress={setUrlAddress}
+      />
+      <PlayGround
+        headersRequest={headersRequest}
+        graphRequest={graphRequest}
+        variablesRequest={variablesRequest}
+        jsonResponse={jsonResponse}
+        setGraphRequest={setGraphRequest}
+        setHeadersRequest={setHeadersRequest}
+        setVariablesRequest={setVariablesRequest}
+      />
     </div>
   );
 };
