63ca9b98977a93b75829418b59341c9323ddf7be
5kazo4nik <alhimov.oleg2012@yandex.ru>
2023-12-29 17:32:01 +0300

    refactor: remove default test, remove lang slice

--- src/store/RootReducer.ts
+++ src/store/RootReducer.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { api, ApiReducer } from '@/api/api';
-import lang from './reducers/langSlice';
 import docs from './reducers/docsSlice';
 import address from './reducers/addressSlice';
 import { RequestReducer } from './reducers/requestSlice';
@@ -8,7 +7,6 @@ import { ResponseReducer } from '@/store/reducers/responseSlice';
 import toast from './reducers/toastSlice';
 
 export const RootReducer = combineReducers({
-  lang,
   docs,
   address,
   toast,
